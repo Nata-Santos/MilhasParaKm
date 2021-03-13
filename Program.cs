086a0c028290cf00b28eacd155e1b689eb05a4ce
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-03-12 23:25:08 -0300

    atividade milhas para km

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string medida;
+            double milhas, km;
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Entre com a medida (em milhas):");
+            Console.WriteLine("-------------------------------");
+            Console.ResetColor();
+            
+            medida = Console.ReadLine();
+            milhas = Convert.ToDouble(medida);
+            km = milhas * 1.609;
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("---Equivalente---");
+            Console.WriteLine($"{km} Km");
+            Console.ResetColor();
+
+
+
+
+
+            
+
+        }
+    }
+}
